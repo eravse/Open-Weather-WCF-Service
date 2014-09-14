7e843c6a6dff3d97cd3167f04c566c1ed1cf4618
eravse <eravse@gmail.com>
2014-09-14 21:13:00 +0300

    Open Weather WCF
    Open Weather WCF service

---
+++ OpenWeatherWCF/IOpenWeatherService.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace OpenWeatherWCF
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IOpenWeatherService
+    {
+
+        [OperationContract]
+        string GetWeather(WeatherParameter Param);
+
+        [OperationContract]
+        WeatherObject GetWeatherobject(WeatherParameter Param);
+
+        void SaveObject(WeatherObject wObject);
+
+        void GetWeatherWithSave(WeatherParameter Param);
+
+        // TODO: Add your service operations here
+    }
+    public class WeatherParameter
+    {
+        public RequestType RequestType { get; set; }
+        public DataFormat Format { get; set; }
+        public UnitType UnitType { get; set; }
+        public string q { get; set; }
+        public string Lat { get; set; }
+        public string Lon { get; set; }
+    }
+
+    public enum RequestType
+    {
+
+        ByCityName = 0,
+        ByCoordinates = 1,
+        ByCityId = 2
+    }
+
+
+    public enum DataFormat
+    {
+        jSon = 0,
+        Xml = 1,
+        Html = 2,
+        WeatherObject = 3,
+
+    }
+
+    public enum UnitType
+    {
+        Imperial = 0,
+        Metrics = 1
+    }
+
+
+    public class Coord
+    {
+        public double lon { get; set; }
+        public double lat { get; set; }
+    }
+
+    public class Sys
+    {
+        public double message { get; set; }
+        public string country { get; set; }
+        public int sunrise { get; set; }
+        public int sunset { get; set; }
+    }
+
+    public class Weather
+    {
+        public int id { get; set; }
+        public string main { get; set; }
+        public string description { get; set; }
+        public string icon { get; set; }
+    }
+
+    public class Main
+    {
+        public double temp { get; set; }
+        public int pressure { get; set; }
+        public int humidity { get; set; }
+        public double temp_min { get; set; }
+        public double temp_max { get; set; }
+    }
+
+    public class Wind
+    {
+        public double speed { get; set; }
+        public int deg { get; set; }
+    }
+
+    public class Rain
+    {
+        public int __invalid_name__3h { get; set; }
+    }
+
+    public class Clouds
+    {
+        public int all { get; set; }
+    }
+
+    public class WeatherObject
+    {
+        public Coord coord { get; set; }
+        public Sys sys { get; set; }
+        public List<Weather> weather { get; set; }
+        public string @base { get; set; }
+        public Main main { get; set; }
+        public Wind wind { get; set; }
+        public Rain rain { get; set; }
+        public Clouds clouds { get; set; }
+        public int dt { get; set; }
+        public int id { get; set; }
+        public string name { get; set; }
+        public int cod { get; set; }
+    }
+}
---
+++ OpenWeatherWCF/OpenWeatherService.svc.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Web.Script.Serialization;
+
+namespace OpenWeatherWCF
+{
+    
+    public class OpenWeatherService : IOpenWeatherService
+    {
+
+
+        /// <summary>
+        /// THIS ONLY RETURN WEATHEROBJECT.
+        /// IF YOU WANT TO JSON OR XML OR HTML CHANGE RETURN TYPE...
+        /// </summary>
+        /// <param name="RequestType">Please Select from enum</param>
+        /// <param name="Format">Default Json .xml or html.</param>
+        /// <param name="UnitType">metric or imperial </param>
+        /// <param name="q">ID or CityName</param>
+        /// <param name="Lat">empty string</param>
+        /// <param name="Long">empty string</param>
+        /// <returns></returns>
+        public string GetWeather(WeatherParameter Param)
+        {
+
+            var wFormat = "";
+            var RType = "";
+            var UType = "";
+
+
+
+            switch (Param.Format)
+            {
+                case DataFormat.jSon:
+                    wFormat = "mode=json";
+                    break;
+                case DataFormat.Xml:
+                    wFormat = "mode=xml";
+                    break;
+                case DataFormat.Html:
+                    wFormat = "mode=html";
+                    break;
+                default:
+                    break;
+            }
+
+            switch (Param.RequestType)
+            {
+                case RequestType.ByCityName:
+                    RType = "q=" + Param.q;
+                    break;
+                case RequestType.ByCoordinates:
+                    RType = "lat=" + Param.Lat + "&lon=" + Param.Lon;
+                    break;
+                case RequestType.ByCityId:  
+                    RType = "iq="+Param.q;
+                    break;
+                default:
+                    break;
+            }
+
+            switch (Param.UnitType)
+	        {
+		        case UnitType.Imperial:
+                            UType="units=imperial";
+                            break;
+                case UnitType.Metrics:
+                    UType="units=metric";
+                 break;
+                default:
+                break;
+	        }
+
+            
+            var BaseUrl = "http://api.openweathermap.org/data/2.5/weather?"+wFormat+"&"+RType+"&"+UType;
+            var Content = "";
+       
+            WebRequest request = WebRequest.Create(
+             BaseUrl);
+          
+            request.Credentials = CredentialCache.DefaultCredentials;
+         
+            WebResponse response = request.GetResponse();
+         
+            Console.WriteLine(((HttpWebResponse)response).StatusDescription);
+          
+            Stream dataStream = response.GetResponseStream();
+           
+            StreamReader reader = new StreamReader(dataStream);
+       
+            string responseFromServer = reader.ReadToEnd();
+           
+            Content = responseFromServer;
+
+
+
+
+
+            reader.Close();
+            response.Close();
+
+            return Content;
+        }
+
+
+
+
+        /// <summary>
+        /// IF YOU GET WEATHER OBJECT PLEASE SET WEATHER PARAMETER FORMAT JSON
+        /// </summary>
+        /// <param name="Param"></param>
+        /// <returns></returns>
+        public WeatherObject GetWeatherobject(WeatherParameter Param)
+        {
+            var jsonObject = GetWeather(Param);
+            WeatherObject WObject = new JavaScriptSerializer().Deserialize<WeatherObject>(jsonObject);
+            return WObject;
+        }
+
+
+
+
+        public void SaveObject(WeatherObject wObject)
+        {
+           
+        }
+
+
+        public void GetWeatherWithSave(WeatherParameter Param)
+        {
+            var jsonObject = GetWeather(Param);
+            WeatherObject WObject = new JavaScriptSerializer().Deserialize<WeatherObject>(jsonObject);
+
+
+            SaveObject(WObject);
+
+        }
+    }
+}
